599d9da235ebce00e27f425e9b9cb379929a3f5e
Francisco Lucas Janesch Lange Sens <franciscosensaulas@gmail.com>
2026-09-11 20:50:48 -0300

    Aula

--- api/alembic/env.py
+++ api/alembic/env.py
@@ -24,7 +24,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -50,6 +50,7 @@ def run_migrations_offline() -> None:
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
+        compare_type=True,
     )
 
     with context.begin_transaction():
@@ -71,7 +72,7 @@ def run_migrations_online() -> None:
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=connection, target_metadata=target_metadata, compare_type=True,
         )
 
         with context.begin_transaction():
--- api/app/core/database.py
+++ api/app/core/database.py
@@ -18,3 +18,10 @@ SessionLocal = sessionmaker(
 # Base é a classe mãe de todos os models SQLAlchemy
 class Base(DeclarativeBase):
     pass
+
+# py -c "import app.models as m; print(sorted(m.Base.metadata.tables))"
+
+# Criar migration
+# alembic revision --autogenerate -m "<mensagem>"
+# Aplicar as migrations (criar tabela, modificar tabelas, apagar tabelas)
+# alembic upgrade head
