b74a05707ec4c27bafda04e1b87e983f2e16a48c
Francisco Lucas Janesch Lange Sens <franciscosensaulas@gmail.com>
2026-09-16 20:53:06 -0300

    feature: categorias

--- api/app/main.py
+++ api/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.core.exceptions import registrar_handler
 from app.controllers.usuario_controller import router as usuario_router
+from app.controllers.categoria_controller import router as categoria_router
 
 
 app = FastAPI()
@@ -8,6 +9,7 @@ app = FastAPI()
 # Traduz as exceções de domínio (app/core/exceptions.py) para respostas HTTP padronizadas
 registrar_handler(app)
 
+app.include_router(categoria_router)
 app.include_router(usuario_router)
 
 # Executar
