6092fbd7408a2d3957dd19e70f6a540607e630d5
Vitor Prange <12965@proway.treina>
2026-09-18 21:18:51 -0300

    feature: tickets

--- api/app/core/enums.py
+++ api/app/core/enums.py
@@ -11,9 +11,17 @@ class StatusChamado(str, Enum):
     EM_ANALISE = "EM_ANALISE"
     RESOLVIDO = "RESOLVIDO"
     CANCELADO = "CANCELADO"
-    
-    
+
+
 class PrioridadeChamado(str, Enum):
     BAIXA = "BAIXA"
     MEDIA = "MEDIA"
     ALTA = "ALTA"
+
+
+class SetorChamado(str, Enum):
+    TI = "TI"
+    RH = "RH"
+    FINANCEIRO = "FINANCEIRO"
+    ADMINISTRATIVO = "ADMINISTRATIVO"
+    MANUTENCAO = "MANUTENCAO"
