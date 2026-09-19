36dddbed054383857ab79a7bd33122f13c799c39
Vitor Prange <12965@proway.treina>
2026-09-18 22:01:56 -0300

    feature: ticket controller, schema, service

--- api/app/controllers/ticket_controller.py
+++ api/app/controllers/ticket_controller.py
@@ -1,7 +1,8 @@
 from fastapi.routing import APIRouter
+from websockets.sync.router import route
 
 from app.dependencies.database import DbSession
-from app.schemas.ticket_schema import TicketCriar, TicketResposta
+from app.schemas.ticket_schema import TicketCriar, TicketResposta, TicketDefinirPrioridade
 from app.services.ticket_service import TicketService
 
 
@@ -10,3 +11,7 @@ router = APIRouter(prefix="/tickets", tags=["Tickets"])
 @router.post("", response_model=TicketResposta)
 def criar(dado: TicketCriar, db: DbSession):
     return TicketService(db).criar(dado)
+
+@router.post("/{id}/definir-prioridade", response_model=TicketResposta)
+def definir_prioridade(id: int, dado: TicketDefinirPrioridade, db: DbSession):
+    return TicketService(db).definir_prioridade(id, dado)
--- api/app/services/ticket_service.py
+++ api/app/services/ticket_service.py
@@ -1,10 +1,11 @@
 from sqlalchemy.orm import Session
 
 from app.core.enums import Papel, StatusChamado
-from app.core.exceptions import PermissaoNegadaError, RegraNegocioError
+from app.core.exceptions import PermissaoNegadaError, RegraNegocioError, NaoEncontradoError
+from app.core.tempo import agora
 from app.models.ticket import Ticket
 from app.repositories.ticket_repository import TicketRepository
-from app.schemas.ticket_schema import TicketCriar
+from app.schemas.ticket_schema import TicketCriar, TicketDefinirPrioridade
 from app.services.usuario_service import UsuarioService
 
 
@@ -29,5 +30,34 @@ class TicketService:
             numero_protocolo="20260918-00001"
         )
         self.ticket_repository.adicionar(ticket)
+        self.db.commit()
+        return ticket
+
+
+    def obter_por_id(self, id: int) -> Ticket:
+        ticket = self.ticket_repository.obter_por_id(id)
+        if ticket is None:
+            raise NaoEncontradoError("Ticket não encontrado")
+        return ticket
+
+    def definir_prioridade(self, id: int, dado: TicketDefinirPrioridade) -> Ticket:
+        # buscar ticket no banco e valida que existe
+        ticket = self.obter_por_id(id)
+
+        #buscar usuario no banco e valida se existe
+        usuario = self.usuario_service.obter_por_id(dado.id_usuario)
+
+        #verificar se papel do usuario é ATENDENTE pois so ele pode abrir tickets
+        if usuario.papel != Papel.ATENDENTE:
+            raise PermissaoNegadaError("Ticket pode ser definido prioridade somente por ATENDENTE")
+
+        ticket.prioridade = dado.prioridade
+
+        # define quem sera o atendente
+        ticket.atendente_id = dado.id_usuario
+
+        ticket.data_atualizacao = agora()
+
+        # salva as modificações
         self.db.commit()
         return ticket
