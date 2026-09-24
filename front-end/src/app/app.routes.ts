5ccba2a8be836086e923278ad1bfc32ead9abf92
Vitor Prange <12965@proway.treina>
2026-09-23 22:02:59 -0300

    feat: cadastro

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {path: "tickets", loadComponent: () => import("./pages/tickets/listar/listar").then(m => m.Listar)}
+    {path: "tickets", loadComponent: () => import("./pages/tickets/listar/listar").then(m => m.Listar)},
+    {path: "tickets/cadastro", loadComponent: () => import("./pages/tickets/cadastro/cadastro").then(m => m.Cadastro)},
 ];
